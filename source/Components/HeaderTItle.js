75a3005fed2f319dd52ce170e2dab83921e88889
Arabhya07092007 <r.arabhaya@gmail.com>
2024-01-02 20:59:38 +0530

    completed the lost and found emergency screen with backend integration

--- source/Components/HeaderTItle.js
+++ source/Components/HeaderTItle.js
@@ -1,21 +1,21 @@
 import * as React from 'react';
-import {Text, View, TouchableOpacity, StyleSheet, Image} from 'react-native';
+import { Text, View, TouchableOpacity, StyleSheet, Image } from 'react-native';
 
 export default class HeaderTitle extends React.Component {
   render() {
     return (
       <View style={styles.logoCont}>
-        <Image
+        {/* <Image
           source={
 
-            {
-                uri: 'https://seeklogo.com/images/U/uttar-pradesh-government-logo-1FA161CB94-seeklogo.com.png',
-              }
-            // require("../Assests/ada_img.png")
+            // {
+            //   uri: 'https://seeklogo.com/images/U/uttar-pradesh-government-logo-1FA161CB94-seeklogo.com.png',
+            // }
+            require("../Assests/ada_img.png")
           }
           style={styles.govLogo}
-        />
-        <Text style={styles.logoHeading}>अयोध्या दर्शन</Text>
+        /> */}
+        <Text style={styles.logoHeading}>Emergency</Text>
       </View>
     );
   }
@@ -25,13 +25,13 @@ const styles = StyleSheet.create({
   govLogo: {
     width: 40,
     height: 40,
-    backgroundColor:"white",
-    borderRadius:100,
-    padding:2
+    backgroundColor: "white",
+    borderRadius: 100,
+    padding: 2
   },
   logoHeading: {
     // fontWeight: 'bold',
-    color: 'white',
+    color: 'black',
     fontSize: 25,
     marginLeft: 5,
     // fontFamily: 'Baloo2-Bold',
--- source/screens/Emergency.js
+++ source/screens/Emergency.js
@@ -14,16 +14,65 @@ import {
     TouchableWithoutFeedback,
     Keyboard,
     ScrollView,
+    ToastAndroid
 } from 'react-native';
 import Icon, { Icons } from '../Components/Icons';
 import EmContact from '../Components/EmContact';
-import LostFound from '../Components/lstFound';
+import { Button, Overlay } from 'react-native-elements';
+import database from '@react-native-firebase/database';
 
-export default function Emergency() {
+export default function Emergency({ navigation }) {
     const [name, setName] = useState('');
     const [phoneNo, setPhoneNo] = useState('');
     const [address, setAddress] = useState('');
     const [complaint, setComplaint] = useState('');
+    const [visible, setVisible] = useState(false);
+    const [uid, setUid] = useState('gegegee');
+
+    const [valid, setValid] = useState(false);
+    const [crName, setCrName] = useState(false);
+    const [crPhoneNo, setCrPhoneNo] = useState(false);
+    const [crAddress, setCrAddress] = useState(false);
+    const [crComplaint, setCrComplaint] = useState(false);
+
+    const showToast = () => {
+        ToastAndroid.showWithGravity(
+            'Enter details properly',
+            ToastAndroid.LONG,
+            ToastAndroid.BOTTOM,
+        );
+    };
+
+    function pushComplaint(name, phoneNo, address, complaint) {
+
+        const data = {
+            name: name,
+            phoneNo: phoneNo,
+            address: address,
+            complaint: complaint,
+            uid: uid,
+        }
+
+        const newReference = database().ref('/Parking/complaints/').push();
+
+        console.log('Auto generated key: ', newReference.key);
+
+        newReference
+            .set(data)
+            .then(() => console.log('Data updated.'));
+
+        setName('');
+        setPhoneNo('');
+        setAddress('');
+        setComplaint('');
+
+
+    }
+
+    const toggleOverlay = () => {
+        setVisible(!visible);
+    };
+
 
     return (
         <SafeAreaView style={styles.cont}>
@@ -32,31 +81,25 @@ export default function Emergency() {
             <View
                 style={{
                     flexDirection: 'row',
-                    marginTop: 20,
+                    marginTop: 10,
                     justifyContent: 'space-between',
+                    // backgroundColor: '#FFFEDE',
                 }}>
                 <View style={{ flexDirection: 'row' }}>
-                    <TouchableOpacity style={{ marginRight: 10 }}>
+                    <TouchableOpacity style={{ marginRight: 20 }} onPress={() => navigation.goBack()}>
                         <Icon
-                            type={Icons.Ionicons}
-                            name={'arrow-back-outline'}
+                            type={Icons.Feather}
+                            name={'arrow-left'}
                             color="black"
                             size={28}
                         />
                     </TouchableOpacity>
-                    <Text style={{ color: 'black', fontSize: 20, fontWeight: '700' }}>
+                    <Text style={{ color: 'black', fontSize: 20, fontWeight: '600' }}>
                         Emergency
                     </Text>
                 </View>
 
-                <TouchableOpacity style={{ marginLeft: 10 }}>
-                    <Icon
-                        type={Icons.Ionicons}
-                        name={'notifications-outline'}
-                        color="black"
-                        size={28}
-                    />
-                </TouchableOpacity>
+
             </View>
 
             <ScrollView>
@@ -85,7 +128,15 @@ export default function Emergency() {
                     </View>
                 </View>
 
-                <View style={{ flex: 1, justifyContent: 'center', marginTop: 20, marginBottom: 20 }}>
+                <View
+                    style={{
+                        flex: 1,
+                        justifyContent: 'center',
+                        marginTop: 10,
+                        marginBottom: 20,
+                    }}>
+
+                    <Text style={{ fontSize: 16, fontWeight: '600', color: 'black', marginBottom: 10 }}>Register for lost and found</Text>
                     <View
                         style={{
                             backgroundColor: 'white',
@@ -98,135 +149,145 @@ export default function Emergency() {
                         <View style={{}}>
                             <View style={{ flexDirection: 'row' }}>
                                 <Text
-                                    style={{
+                                    style={[crName == true ? { color: 'red' } : { color: 'black' }, {
                                         backgroundColor: 'white',
-                                        color: 'black',
                                         padding: 5,
                                         top: 12,
                                         zIndex: 1000,
                                         marginLeft: 20,
                                         fontSize: 14,
-                                    }}>
+                                    }]}>
                                     Name
                                 </Text>
                                 <View style={{ flex: 1 }}></View>
                             </View>
                             <TextInput
-                                style={{
+                                style={[crName == true ? { borderColor: 'red' } : { borderColor: '#d1cfcf' }, {
                                     borderWidth: 1.2,
                                     padding: 10,
                                     borderRadius: 10,
-                                    fontWeight: '500',
-                                    fontSize: 15,
                                     marginHorizontal: 10,
-                                }}
+                                    color: 'black'
+                                }]}
                                 placeholder="Enter Your Name"
                                 placeholderTextColor="#888"
                                 value={name}
-                                onChangeText={(text) => setName(text)}
+                                onChangeText={(text) => {
+                                    setName(text);
+                                    setCrName(false);
+                                }}
                             />
                         </View>
 
                         <View>
                             <View style={{ flexDirection: 'row' }}>
                                 <Text
-                                    style={{
+                                    style={[crPhoneNo == true ? { color: 'red' } : { color: 'black' }, {
                                         backgroundColor: 'white',
-                                        color: 'black',
                                         padding: 5,
                                         top: 12,
                                         zIndex: 1000,
                                         marginLeft: 20,
                                         fontSize: 14,
-                                    }}>
+                                    }]}>
                                     Phone Number
                                 </Text>
                                 <View style={{ flex: 1 }}></View>
                             </View>
                             <TextInput
-                                style={{
+                                style={[crPhoneNo == true ? { borderColor: 'red' } : { borderColor: '#d1cfcf' }, {
                                     borderWidth: 1.2,
                                     padding: 10,
                                     borderRadius: 10,
-                                    fontWeight: '500',
-                                    fontSize: 15,
                                     marginHorizontal: 10,
-                                }}
+                                    color: 'black'
+                                }]}
                                 placeholder="Enter Your Phone Number"
                                 placeholderTextColor="#888"
                                 value={phoneNo}
-                                onChangeText={(text) => setPhoneNo(text)}
+                                onChangeText={(text) => { setPhoneNo(text); setCrPhoneNo(false); }}
                             />
                         </View>
 
                         <View>
                             <View style={{ flexDirection: 'row' }}>
                                 <Text
-                                    style={{
+                                    style={[crAddress == true ? { color: 'red' } : { color: 'black' }, {
                                         backgroundColor: 'white',
-                                        color: 'black',
                                         padding: 5,
                                         top: 12,
                                         zIndex: 1000,
                                         marginLeft: 20,
                                         fontSize: 14,
-                                    }}>
+                                    }]}>
                                     Address
                                 </Text>
                                 <View style={{ flex: 1 }}></View>
                             </View>
                             <TextInput
-                                style={{
+                                style={[crAddress == true ? { borderColor: 'red' } : { borderColor: '#d1cfcf' }, {
                                     borderWidth: 1.2,
                                     padding: 10,
                                     borderRadius: 10,
-                                    fontWeight: '500',
-                                    fontSize: 15,
                                     marginHorizontal: 10,
-                                }}
+                                    color: 'black'
+                                }]}
                                 placeholder="Enter Your Address"
                                 placeholderTextColor="#888"
                                 value={address}
-                                onChangeText={(text) => setAddress(text)}
+                                onChangeText={(text) => { setAddress(text); setCrAddress(false); }}
                             />
                         </View>
 
                         <View>
                             <View style={{ flexDirection: 'row' }}>
                                 <Text
-                                    style={{
+                                    style={[crComplaint == true ? { color: 'red' } : { color: 'black' }, {
                                         backgroundColor: 'white',
-                                        color: 'black',
                                         padding: 5,
                                         top: 12,
                                         zIndex: 1000,
                                         marginLeft: 20,
                                         fontSize: 14,
-                                    }}>
+                                    }]}>
                                     Complaint
                                 </Text>
                                 <View style={{ flex: 1 }}></View>
                             </View>
                             <TextInput
-                                style={{
+                                style={[crComplaint == true ? { borderColor: 'red' } : { borderColor: '#d1cfcf' }, {
                                     borderWidth: 1.2,
                                     padding: 10,
                                     borderRadius: 10,
-                                    fontWeight: '500',
-                                    fontSize: 15,
                                     marginHorizontal: 10,
-                                    color: '#888',
-                                }}
+                                    color: 'black'
+                                }]}
                                 placeholder="Enter Your Detailed complaint with place 
 and time "
                                 placeholderTextColor="#888"
                                 value={complaint}
-                                onChangeText={(text) => setComplaint(text)}
+                                onChangeText={(text) => { setComplaint(text), setCrComplaint(false); }}
                             />
                         </View>
 
                         <View style={{ marginVertical: 15 }}>
                             <TouchableOpacity
+                                // onPress={toggleOverlay}
+                                onPress={() => {
+                                    if (name !== '' && phoneNo !== '' && address !== '' && complaint !== '') {
+                                        pushComplaint(name, phoneNo, address, complaint);
+                                        toggleOverlay();
+                                    } else {
+                                        setValid(true);
+                                        if (name === '') setCrName(true);
+                                        if (phoneNo === '') setCrPhoneNo(true);
+                                        if (address === '') setCrAddress(true);
+                                        if (complaint === '') setCrComplaint(true);
+
+                                        showToast();
+
+                                    }
+                                }}
                                 style={{
                                     backgroundColor: '#B01432',
                                     width: 200,
@@ -249,6 +310,42 @@ and time "
                     </View>
                 </View>
             </ScrollView>
+
+            <Overlay isVisible={visible} onBackdropPress={toggleOverlay}>
+                <View style={{ alignItems: "center", padding: 20 }}>
+                    <Image
+                        source={require('../Assests/Success.png')}
+                        style={{ width: 157, height: 201 }}
+                    />
+
+                    <Text
+                        style={{
+                            fontSize: 20,
+                            fontWeight: '700',
+                            alignSelf: 'center',
+                            marginTop: 20,
+                            color: '#B01432',
+                        }}>
+                        Successful!
+                    </Text>
+                    <Text style={{ color: 'black' }}>
+                        Your Complaint report is registered.
+                    </Text>
+                    <Text style={{ color: 'black' }}>
+                        Help is on the way.
+                    </Text>
+
+                    <View style={{ marginTop: 20 }}>
+                        <TouchableOpacity style={{ backgroundColor: "#F0A936", padding: 10, borderRadius: 10, elevation: 0.5, paddingHorizontal: 30 }}>
+                            <Text style={{ color: "#B01432", fontWeight: "500", textAlign: 'center' }}>Track Progress</Text>
+                        </TouchableOpacity>
+
+                        <TouchableOpacity onPress={toggleOverlay} style={{ backgroundColor: "#FFFEDE", marginTop: 10, padding: 10, borderRadius: 10, paddingHorizontal: 30, borderWidth: 1, borderColor: "#F0A936" }}>
+                            <Text style={{ color: "#B01432", fontWeight: "500", textAlign: 'center' }}>Cancel</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Overlay>
         </SafeAreaView>
     );
 }
@@ -257,6 +354,6 @@ const styles = StyleSheet.create({
     cont: {
         flex: 1,
         paddingHorizontal: 10,
-        backgroundColor: '#FDFAE7',
+        backgroundColor: '#FFFEDE',
     },
 });
